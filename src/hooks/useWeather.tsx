1ef8a2a8bf2f08617d45e4cb5c8fd73901fec696
luisgoweb <luismga2012@gmail.com>
2025-07-22 14:52:21 -0400

    Se mueven las credenciales de la API a una variable de entorno

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -8,7 +8,7 @@ export const useWeather = () => {
         
 
         try {
-            const appId = '5738334d7396873b0bda0ea4824d0ab7'
+            const appId = import.meta.env.VITE_API_KEY
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
             const {data} = await axios(geoUrl)
