aec073f2beb68c534b0ef5205d79bcacef61d861
luisgoweb <luismga2012@gmail.com>
2025-07-22 11:49:47 -0400

    Form.module.css: Se añaden los estilos al formulario

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,10 +1,11 @@
 import { countries } from "../../data/countries"
+import styles from "./Form.module.css"
 
 
 const Form = () => {
   return (
-    <form>
-        <div>
+    <form className={styles.form} >
+        <div className={styles.field}>
             <label htmlFor="city">Ciudad:</label>
             <input 
             id="city"
@@ -13,7 +14,7 @@ const Form = () => {
             />
         </div>
 
-        <div>
+        <div className={styles.field}>
             <label htmlFor="city">País:</label>
             <select 
             id="city"
